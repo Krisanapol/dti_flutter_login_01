b5b6b7e3fb3f0a19e53a4b300801e324b15201f5
Krisanapol <s6452410015@sau.ac.th>
2023-08-23 16:49:27 +0700

    Time Pong Die

---
+++ lib/views/login_b_ui.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+    );
+  }
+}
